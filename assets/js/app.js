6798175767a3cf32571d93d583bb305e99adc30d
Umut Tosun <tosun.umutt@gmail.com>
2023-02-16 16:54:11 +0300

    yeni fonksiyon ve tasarımlar eklendi.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,34 +4,43 @@ var max = 10;
 var level = 1;
 var randomNumber;
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
 function gameStart() {
-/*Muhammet Çelikçi
-1-) Bu fonksiyon çalıştıgı zaman başlat panelindeki kısımlar gizlenecektir.
-2-) Kullanıcının sayı girmesi için önceden hazırlanmış input ekrana gelicektir
-3-) Onayla diye bir buton olucaktır
-4-) Üst kısımda bilgiler olucaktır
-*/
+    document.getElementById('gameStart-items').style.display = "none";
+    document.getElementById('btn_gameStart').style.display = "none";
+    document.getElementById('text').style.display = "block";
+    delay(1000).then(() => 
+     { 
+     document.getElementById('inputBox').style.display = "block";
+     document.getElementById('btn_gameStart').style.display = "block"
+     document.getElementById('text').style.display = "none";
+     });
+
 }
 function getRandomNumber() {
     /*Umut Tosun*/
     minValue *= 10;
     level++;
     max = 1;
-    document.getElementById('text').innerText=Math.floor(Math.random() * ((max - 1) - minValue)) + minValue;
+    document.getElementById('text').innerText = Math.floor(Math.random() * ((max - 1) - minValue)) + minValue;
     for (var i = 1; i <= level; i++)  max = max * 10;
     return Math.floor(Math.random() * ((max - 2) - minValue)) + minValue;
 }
 function gameOver() {
-/*Adem Eraslan
-https://humanbenchmark.com/tests/number-memory burdaki mantıgın aynısı yapılıcak
-*/
+    /*Adem Eraslan
+    https://humanbenchmark.com/tests/number-memory burdaki mantıgın aynısı yapılıcak
+    */
 }
 function getResult(input) {
-/*Onur Işık*/
+    /*Onur Işık*/
 }
-function restartGame(){
+function restartGame() {
     /*Can Tarakçı*/
 }
-function nextLevel(){
+function nextLevel() {
     /*Umut Tosun*/
 }
